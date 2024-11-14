c2caa74a307634b3ed430594890180d300ccd800
Ant <toha2217@mail.ru>
2024-11-14 22:04:13 +0300

    Added program

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿Console.Write("Enter your array of strings separated by a space: ");
+string? inputstr = Console.ReadLine();
+string [] arrays = inputstr.Split(' ');
+int j = 0;
+string [] str = new string[arrays.Length];
+for (int i = 0; i < arrays.Length; i++)
+{
+    if (arrays[i].Length <= 3)
+    {
+    str[j] = arrays[i]; 
+    j++;
+    }
+}
+for (int e = 0; e < str.Length; e++)
+{
+    Console.Write($"{str[e]} ");
+}
