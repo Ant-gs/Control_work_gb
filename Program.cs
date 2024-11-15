7555b8cb23111f4d92e7a39512592ab893dff328
Ant <toha2217@mail.ru>
2024-11-16 02:32:50 +0300

    Modifired prog and added gitignore

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
 ﻿Console.Write("Enter your array of strings separated by a space: ");
 string? inputstr = Console.ReadLine();
 string [] arrays = inputstr.Split(' ');
-int j = 0;
-string [] str = new string[arrays.Length];
+int j = 0, counter = 0;
+for (int i = 0; i < arrays.Length; i++)
+{
+    if (arrays[i].Length <= 3)
+    {
+        counter++;
+    }
+}
+string [] str = new string[counter];
 for (int i = 0; i < arrays.Length; i++)
 {
     if (arrays[i].Length <= 3)
@@ -11,7 +18,8 @@ for (int i = 0; i < arrays.Length; i++)
     j++;
     }
 }
+Console.WriteLine("A new array whose elements contain no more than 3 characters: ");
 for (int e = 0; e < str.Length; e++)
 {
-    Console.Write($"{str[e]} ");
+    Console.Write($"[{str[e]}] ");
 }
